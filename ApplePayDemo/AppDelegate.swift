e6dd484b19140684708c800607431a8649e0624f
Jack Flintermann <jack@stripe.com>
2014-12-05 17:28:09 -0500

    small design improvements

--- ApplePayDemo/AppDelegate.swift
+++ ApplePayDemo/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        application.setStatusBarStyle(UIStatusBarStyle.LightContent, animated: false)
         return true
     }
     
